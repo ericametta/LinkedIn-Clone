a4d493af6e279072b984b7d3c4c7cf09a62962c4
Erica Metta <ericamettaz@gmail.com>
2023-03-13 02:15:11 -0400

    login update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
-import { selectUser } from './features/counter/userSlice';
+import { login, logout, selectUser } from './features/counter/userSlice';
 import Feed from './Feed';
 import Header from './Header';
 import Login from './Login';
 import Sidebar from './Sidebar';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 
 function App() {
-  const user = useSelector(selectUser)
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch;
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (userCredential) => {
+      if (userCredential) {
+        dispatch(login({
+          email: userCredential.user.email,
+          uid: userCredential.user.uid,
+          displayName: userCredential.displayName,
+          // display name and profile picture 
+      }))
+      } else {
+        dispatch(logout());
+      }
+
+    });
+  
+  }, []);
 
   return (
     <div className="app">
--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,40 @@
+import { useState } from "react";
 import "./Login.css";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+import { login } from "./features/counter/userSlice";
+
 
 const Login = () => {
-    const loginToApp = () => (
-        <div>Login</div>
-    );
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    // const [user, setUser] = useState(null);
+    const dispatch = useDispatch();
+
+
+
+    const loginToApp = (e) => {
+        e.preventDefault();
+        
+    };
 
     const register = () => (
-            <div>Login</div>
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+                dispatch(login({
+                    email: userCredential.user.email,
+                    uid: userCredential.user.uid,
+                    displayName: email,
+                    // display name and profile picture 
+                }))
+            })
+        .catch((error) => {
+            console.error(error);
+            alert(error);
+        })
     );
+    
     
 
   return (
@@ -21,8 +48,8 @@ const Login = () => {
                 <p>Stay updated on your professional world</p>
             </div>
             <form>
-                <input type="text" placeholder="Email or Phone"/>
-                <input type="password" placeholder="Password"/>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" placeholder="Email or Phone" />
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
                 <h4>Forgot password?</h4>
                 <button type="submit" onClick={loginToApp}>Sign In</button>
             </form>
